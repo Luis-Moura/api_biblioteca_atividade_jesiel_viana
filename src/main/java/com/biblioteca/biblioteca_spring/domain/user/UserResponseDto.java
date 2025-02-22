7c828fa44336a946dd120f8d2aa2ddc2a717a68b
Luis-Moura <luish.moura2024@gmail.com>
2025-02-22 20:23:07 -0300

    feat: simplify UserResponseDto by using Lombok annotations for constructors

--- src/main/java/com/biblioteca/biblioteca_spring/domain/user/UserResponseDto.java
+++ src/main/java/com/biblioteca/biblioteca_spring/domain/user/UserResponseDto.java
@@ -1,16 +1,15 @@
 package com.biblioteca.biblioteca_spring.domain.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
     private String name;
     private String email;
-
-    public UserResponseDto(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
 }
