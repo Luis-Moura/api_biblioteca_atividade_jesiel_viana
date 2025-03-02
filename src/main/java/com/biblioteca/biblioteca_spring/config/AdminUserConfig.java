332abeb63658c771addd217cdea32ec4ab9519c8
Luis-Moura <luish.moura2024@gmail.com>
2025-03-02 13:11:20 -0300

    feat: add AdminUserConfig and SecurityConfig for user management and JWT authentication

---
+++ src/main/java/com/biblioteca/biblioteca_spring/config/AdminUserConfig.java
@@ -0,0 +1,37 @@
+package com.biblioteca.biblioteca_spring.config;
+
+import com.biblioteca.biblioteca_spring.domain.user.User;
+import com.biblioteca.biblioteca_spring.domain.user.UserRepository;
+import com.biblioteca.biblioteca_spring.domain.user.UserRoles;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AdminUserConfig {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostConstruct
+    public void run() {
+        var userAdmin = this.userRepository.findByEmail("admin");
+
+        userAdmin.ifPresentOrElse(
+                (user) -> {
+                    System.out.println("Admin Already Exists");
+                },
+                () -> {
+                    var user = new User();
+                    user.setName("admin");
+                    user.setEmail("admin");
+                    user.setPassword(bCryptPasswordEncoder.encode("blabla")); //tirar esse hardcode aqui
+                    user.setUserRoles(UserRoles.ADMIN);
+                    userRepository.save(user);
+                }
+        );
+    }
+}
